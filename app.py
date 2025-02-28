68a34e0bf41ba59cbbdcbc450558b7f5c38f6179
Anderson Neo <andersonneo113@gmail.com>
2025-02-27 22:07:37 -0300

    baixar somente o audio

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 import yt_dlp 
-import streamlit as st  
-st.title("Baixar vídeos do YouTube") 
-urlDoVideo = st.text_input("Digite a URL do vídeo do YouTube") 
-baixar = st.button("Baixar")  
+import streamlit as st 
 
-if baixar and urlDoVideo:
-         pasta_downloads = "downloads"
-         ydl_opts = {         
+st.title("Baixar vídeos e áudios do YouTube") 
+urlDoVideo = st.text_input("Digite a URL do vídeo do YouTube") 
+
+col1, col2 = st.columns([1, 8])
+with col1:
+    baixarVideo = st.button("Baixar")
+
+with col2:
+    baixarAudio = st.button("Baixar apenas o áudio")
+
+if baixarVideo and urlDoVideo:
+        pasta_downloads = "downloads"
+        ydl_opts = {         
                  'format': 'bv*[height=1080][ext=mp4]',         
                  'outtmpl': f'{pasta_downloads}/%(title)s.%(ext)s',     
                  }      
-         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                     ydl.download([urlDoVideo])
-                     st.success(f"Download concluído! O vídeo foi salvo na pasta: {pasta_downloads}")
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([urlDoVideo])
+                st.success(f"Download concluído! O vídeo foi salvo na pasta: {pasta_downloads}")
+
+if baixarAudio and urlDoVideo:
+        pasta_downloads = "downloads"
+        ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': f'{pasta_downloads}/%(title)s.%(ext)s',
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([urlDoVideo])
+                st.success(f"Download concluído! O áudio foi salvo na pasta: {pasta_downloads}")
